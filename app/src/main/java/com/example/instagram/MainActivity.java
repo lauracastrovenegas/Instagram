94995bf20adbd0848bc2fe576d070a35ef536967
Laura Castro Venegas <laurasofi1024@gmail.com>
2021-07-06 18:57:23 -0400

    Implemented post query

--- app/src/main/java/com/example/instagram/MainActivity.java
+++ app/src/main/java/com/example/instagram/MainActivity.java
@@ -4,17 +4,60 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 import com.example.instagram.ui.login.LoginActivity;
+import com.parse.FindCallback;
+import com.parse.GetCallback;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
 import com.parse.ParseUser;
 
+import java.util.List;
+
+import models.Post;
+
 public class MainActivity extends AppCompatActivity {
+    
+    public static final String TAG = "MainActivity";
+    private EditText etDescription;
+    private Button btnCapture;
+    private ImageView ivPostImage;
+    private Button btnSubmit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        etDescription = findViewById(R.id.etDescription);
+        btnCapture = findViewById(R.id.btnCapture);
+        ivPostImage = findViewById(R.id.ivPostImage);
+        btnSubmit = findViewById(R.id.btnSubmit);
+        
+        queryPosts();
+    }
+
+    private void queryPosts() {
+        ParseQuery<Post> query = ParseQuery.getQuery(Post.class);
+        query.include(Post.KEY_USER);
+        query.findInBackground(new FindCallback<Post>() {
+            @Override
+            public void done(List<Post> posts, ParseException e) {
+                if (e != null){
+                    Log.e(TAG, "Issue with getting posts", e);
+                    return;
+                }
+
+                for (Post post : posts){
+                    Log.i(TAG, "Post: " + post.getDescription() + ", username:" + post.getUser().getUsername());
+                }
+            }
+        });
     }
 
     public void onLogout(View view){
