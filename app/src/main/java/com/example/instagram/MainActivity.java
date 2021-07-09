6a11999c017a1d0fd333218db1f8d4248940d8ba
Laura Castro Venegas <laurasofi1024@gmail.com>
2021-07-09 17:37:51 -0400

    fixed menu icons for selected state, removed labels

--- app/src/main/java/com/example/instagram/MainActivity.java
+++ app/src/main/java/com/example/instagram/MainActivity.java
@@ -50,7 +50,7 @@ public class MainActivity extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
-
+        navView.setItemIconTintList(null);
         NavigationUI.setupWithNavController(navView, navController);
 
     }
